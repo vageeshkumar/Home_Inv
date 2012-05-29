533b21508bb76c27620be4e2e1eeae46035bd86c
Vageesh Kumar <vageeshkumar@gmail.com>
2012-05-29 12:51:00 -0500

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ HomeInv::Application.routes.draw do
   get '/categories' => 'categories#index', :as => :categories  
   get '/categories/new' => 'categories#new', :as => :categories_new
   post '/categories' => 'categories#create', :as => :categories_create
-  get '/categories/:id' => 'categories#show', :as => :categories_individual
+  get '/categories/:id' => 'categories#show', :as => :category
   delete '/categories/:id' => 'categories#destroy', :as => :categories_delete
   get '/categories/:id/edit' => 'categories#edit', :as => :categories_edit
 
